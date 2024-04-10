84eea08a5b813411e6c32e09695722b1906a012e
Salvador <70590860+salvador-palma@users.noreply.github.com>
2024-04-10 14:23:14 +0100

    Semana 7 - Css e Templates

---
+++ Semana7/siteIscte/siteIscte/votacao/models.py
@@ -0,0 +1,28 @@
+import datetime
+
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+class Questao(models.Model):
+     questao_texto = models.CharField(max_length=200)
+     pub_data = models.DateTimeField('data de publicacao')
+
+     def __str__(self):
+          return self.questao_texto
+     def publicada_recentemente(self):
+          return timezone.now() - datetime.timedelta(days=1) <= self.pub_data
+
+class Opcao(models.Model):
+     questao = models.ForeignKey(Questao,on_delete=models.CASCADE)
+     opcao_texto = models.CharField(max_length=200)
+     votos = models.IntegerField(default=0)
+
+     def __str__(self):
+          return self.opcao_texto
+
+class Aluno(models.Model):
+     user = models.OneToOneField(User, on_delete=models.CASCADE)
+     curso = models.CharField(max_length=100)
+     total_votos = models.IntegerField(default=0)
---
+++ Semana7/siteIscte/siteIscte/votacao/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include, path
+from . import views
+
+app_name = 'votacao'
+urlpatterns = [
+    path("", views.index, name='index'),
+    path('<int:questao_id>', views.detalhe, name='detalhe'),
+    path('<int:questao_id>/resultados', views.resultados, name='resultados'),
+    path('<int:questao_id>/voto', views.voto, name='voto'),
+    path('criarquestao', views.criarquestao, name='criarquestao'),
+    path('<int:questao_id>/criaropcao', views.criaropcao, name='criaropcao'),
+    path('<int:questao_id>/apagarquestao', views.apagarquestao, name='apagarquestao'),
+    path("signup", views.signup, name='signup'),
+    path("logoutview", views.logoutview, name='logoutview'),
+    path("profile", views.profile, name='profile')
+]

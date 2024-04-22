5cea5ba26b7e5d531b6e1fcea7f6ebedfa11b00d
Salvador <70590860+salvador-palma@users.noreply.github.com>
2024-04-22 16:03:28 +0100

    semana 8

---
+++ Semana8/siteIscte/siteIscte/votacao/models.py
@@ -0,0 +1,33 @@
+import datetime
+
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+class Questao(models.Model):
+     questao_texto = models.CharField(max_length=200)
+     pub_data = models.DateTimeField('data de publicacao')
+
+     def __str__(self):
+          return self.questao_texto
+     def publicada_recentemente(self):
+          return timezone.now() - datetime.timedelta(days=1) <= self.pub_data
+
+
+
+class Aluno(models.Model):
+     user = models.OneToOneField(User, on_delete=models.CASCADE)
+     curso = models.CharField(max_length=100)
+     avatar = models.CharField(max_length=25, default="AvatarDefault")
+     total_votos = models.IntegerField(default=0)
+
+class Opcao(models.Model):
+     questao = models.ForeignKey(Questao,on_delete=models.CASCADE)
+     opcao_texto = models.CharField(max_length=200)
+     votos = models.IntegerField(default=0)
+
+     votadores = models.ManyToManyField(Aluno, related_name="votos")
+
+     def __str__(self):
+          return self.opcao_texto
